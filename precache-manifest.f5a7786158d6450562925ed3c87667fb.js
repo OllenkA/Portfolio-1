15ec163e5b3445d09669edc6a00fcb5afaf596a3
Ollenka <Ollenka@gmail.com>
2019-10-30 22:28:39 +0300

    Updates

---
+++ precache-manifest.f5a7786158d6450562925ed3c87667fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2265659da23ef57c0674eb3e6606723",
+    "url": "/Portfolio-1/index.html"
+  },
+  {
+    "revision": "8394a6fef01058969e78",
+    "url": "/Portfolio-1/static/css/main.02244d1f.chunk.css"
+  },
+  {
+    "revision": "65590a6c86eb385b0f7a",
+    "url": "/Portfolio-1/static/js/2.b31c1de2.chunk.js"
+  },
+  {
+    "revision": "8394a6fef01058969e78",
+    "url": "/Portfolio-1/static/js/main.32848e2c.chunk.js"
+  },
+  {
+    "revision": "708bb6c7d55f36091e09",
+    "url": "/Portfolio-1/static/js/runtime~main.50905be5.js"
+  },
+  {
+    "revision": "b91cd0ed91f59b1051dcfefc21267376",
+    "url": "/Portfolio-1/static/media/game.b91cd0ed.png"
+  },
+  {
+    "revision": "f8214c27a6fb7c10636031315251c93a",
+    "url": "/Portfolio-1/static/media/goals.f8214c27.png"
+  },
+  {
+    "revision": "7ea12e5736ba8976f897764e83ff4795",
+    "url": "/Portfolio-1/static/media/myPhoto.7ea12e57.jpg"
+  },
+  {
+    "revision": "7bad824676996457191c17c1cded6344",
+    "url": "/Portfolio-1/static/media/pelican1.7bad8246.png"
+  },
+  {
+    "revision": "25bfa4e71969cb32cbaf449ae0fa717f",
+    "url": "/Portfolio-1/static/media/photo.25bfa4e7.jpg"
+  },
+  {
+    "revision": "b421587d12802088089609a355da3ddb",
+    "url": "/Portfolio-1/static/media/timer.b421587d.png"
+  },
+  {
+    "revision": "8265b33c9a7cdb3ac9cd60a5697f89a6",
+    "url": "/Portfolio-1/static/media/todolist.8265b33c.png"
+  }
+]);
